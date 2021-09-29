79d73338bf9a3af2b43d99981724ed2bf2f1f75c
cyruswere <cyrus.were@student.moringschool.com>
2021-09-29 09:14:58 +0300

    made changes

---
+++ app/src/main/java/com/cyruswere/playbeat_4/Constants.java
@@ -0,0 +1,8 @@
+package com.cyruswere.playbeat_4;
+
+public class Constants {
+    public static final String BASE_URL = "https://itunes.apple.com";
+    public static final String FIREBASE_CHILD_SEARCHED_KEY_WORD = "TypedSearchKeyWOrd";
+    public static final String FIREBASE_CHILD_RESULTS = "results";
+    public static final String PREFERENCES_RESULT_KEY = "result";
+}
